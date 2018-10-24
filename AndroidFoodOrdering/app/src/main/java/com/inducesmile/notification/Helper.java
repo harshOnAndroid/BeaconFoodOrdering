8099d5eede4312967327e47806b702eeeb46271d
Unknown <harshyupasani@gmail.com>
2018-10-24 11:53:33 +0530

    first commit

---
+++ AndroidFoodOrdering/app/src/main/java/com/inducesmile/notification/Helper.java
@@ -0,0 +1,9 @@
+package com.inducesmile.notification;
+
+
+public class Helper {
+
+    public static final String PATH_TO_SERVER_TOKEN_STORAGE = "http://localhost/restaurant/templates/register";
+
+    public static final String TOKEN_TO_SERVER = "server_token";
+}
