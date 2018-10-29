72bbd0fbca16d6187b58b67f72f873c9b1b7db6d
harsh upasani <harshyupasani@gmail.com>
2018-10-29 13:22:45 +0530

    gradle bug fixes

--- AndroidFoodOrdering/app/src/main/java/com/inducesmile/androidfoodordering/util/CustomApplication.java
+++ AndroidFoodOrdering/app/src/main/java/com/inducesmile/androidfoodordering/util/CustomApplication.java
@@ -1,40 +1,41 @@
 package com.inducesmile.androidfoodordering.util;
 
-import android.Manifest;
-import android.app.AlertDialog;
 import android.app.Application;
-import android.app.NotificationManager;
-import android.content.DialogInterface;
+import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
-import android.util.Log;
 
+import com.estimote.proximity_sdk.api.EstimoteCloudCredentials;
+import com.estimote.proximity_sdk.api.ProximityObserver;
+import com.estimote.proximity_sdk.api.ProximityObserverBuilder;
+import com.estimote.proximity_sdk.api.ProximityZone;
+import com.estimote.proximity_sdk.api.ProximityZoneBuilder;
+import com.estimote.proximity_sdk.api.ProximityZoneContext;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.inducesmile.androidfoodordering.MainActivity;
+import com.inducesmile.androidfoodordering.IntroActivity;
 import com.inducesmile.androidfoodordering.R;
 import com.inducesmile.androidfoodordering.entities.CartObject;
 import com.inducesmile.androidfoodordering.entities.LoginObject;
 
-import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
-import org.altbeacon.beacon.Region;
-import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
-import org.altbeacon.beacon.startup.BootstrapNotifier;
-import org.altbeacon.beacon.startup.RegionBootstrap;
+import java.util.Set;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
+import static com.inducesmile.androidfoodordering.util.Helper.ESTIMOTE_APP_ID;
+import static com.inducesmile.androidfoodordering.util.Helper.ESTIMOTE_APP_TOKEN;
 
 
-public class CustomApplication extends Application implements BootstrapNotifier
-{
+public class CustomApplication extends Application {
 
+    private static final String CHANNEL_ID = "evvone";
     private Gson gson;
     private GsonBuilder builder;
 
     private CustomSharedPreference shared;
-    private BackgroundPowerSaver backgroundPowerSaver;
+    private ProximityObserver.Handler observationHandler;
 
     @Override
     public void onCreate() {
@@ -44,71 +45,151 @@ public class CustomApplication extends Application implements BootstrapNotifier
         shared = new CustomSharedPreference(getApplicationContext());
     }
 
-    public void initBeacon(){
-        backgroundPowerSaver = new BackgroundPowerSaver(this);
-        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
-        beaconManager.getBeaconParsers().add(new BeaconParser().
-                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+    public void initBeacon() {
+        ProximityZone teaZone;
+        EstimoteCloudCredentials cloudCredentials = new EstimoteCloudCredentials(ESTIMOTE_APP_ID, ESTIMOTE_APP_TOKEN);
+        ProximityObserver proximityObserver = new ProximityObserverBuilder(getApplicationContext(), cloudCredentials)
+                .withBalancedPowerMode()
+                .onError(new Function1<Throwable, Unit>() {
+                    @Override
+                    public Unit invoke(Throwable throwable) {
+                        return null;
+                    }
+                })
+                .build();
 
-        // wake up the app when any beacon is seen (you can specify specific id filers in the parameters below)
-        Region region = new Region("com.inducesmile.androidfoodordering", null, null, null);
-        regionBootstrap = new RegionBootstrap(this, region);
+        teaZone = new ProximityZoneBuilder()
+                .forTag("tea")
+                .inFarRange()
+                .onEnter(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        onZoneEnter("tea");
+                        return null;
+                    }
+                })
+                .onExit(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        onZoneExit();
+                        return null;
+                    }
+                })
+                .onContextChange(new Function1<Set<? extends ProximityZoneContext>, Unit>() {
+                    @Override
+                    public Unit invoke(Set<? extends ProximityZoneContext> proximityZoneContexts) {
+                        return null;
+                    }
+                })
+                .build();
+
+        ProximityZone drinksZone = new ProximityZoneBuilder()
+                .forTag("drinks")
+                .inFarRange()
+                .onEnter(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        onZoneEnter("drinks");
+                        return null;
+                    }
+                })
+                .onExit(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        onZoneExit();
+                        return null;
+                    }
+                })
+                .onContextChange(new Function1<Set<? extends ProximityZoneContext>, Unit>() {
+                    @Override
+                    public Unit invoke(Set<? extends ProximityZoneContext> proximityZoneContexts) {
+                        return null;
+                    }
+                })
+                .build();
+
+        ProximityZone snacksZone = new ProximityZoneBuilder()
+                .forTag("snacks")
+                .inFarRange()
+                .onEnter(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        onZoneEnter("snacks");
+                        return null;
+                    }
+                })
+                .onExit(new Function1<ProximityZoneContext, Unit>() {
+                    @Override
+                    public Unit invoke(ProximityZoneContext proximityContext) {
+                        onZoneExit();
+                        return null;
+                    }
+                })
+                .onContextChange(new Function1<Set<? extends ProximityZoneContext>, Unit>() {
+                    @Override
+                    public Unit invoke(Set<? extends ProximityZoneContext> proximityZoneContexts) {
+                        return null;
+                    }
+                })
+                .build();
+
+        observationHandler = proximityObserver.startObserving(teaZone);
+
     }
 
-    public CustomSharedPreference getShared(){
+    private void onZoneExit() {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(CustomApplication.this)
+                .setSmallIcon(R.drawable.favorite_icon)
+                .setContentTitle("Beacon Exit")
+                .setContentText("You have just exited the beacon region.")
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat.from(CustomApplication.this).notify(0, mBuilder.build());
+    }
+
+    private void onZoneEnter(String tag) {
+        Intent intent = new Intent(CustomApplication.this, IntroActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        CustomApplication.this.startActivity(intent);
+
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1212, intent, PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(CustomApplication.this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.favorite_icon)
+                .setContentTitle("Beacon Exit")
+                .setContentText("You have just exited the beacon region.")
+                .addAction(R.drawable.favorite_icon, "launching app", pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat.from(CustomApplication.this).notify(0, mBuilder.build());
+
+        if (observationHandler != null)
+            observationHandler.stop();
+    }
+
+    public CustomSharedPreference getShared() {
         return shared;
     }
 
-    public Gson getGsonObject(){
+    public Gson getGsonObject() {
         return gson;
     }
 
-    public LoginObject getLoginUser(){
+    public LoginObject getLoginUser() {
         Gson mGson = getGsonObject();
         String storedUser = getShared().getUserData();
         return mGson.fromJson(storedUser, LoginObject.class);
     }
 
-    public int cartItemCount(){
+    public int cartItemCount() {
         String orderList = getShared().getCartItems();
         CartObject[] allCart = getGsonObject().fromJson(orderList, CartObject[].class);
-        if(allCart == null){
+        if (allCart == null) {
             return 0;
         }
         return allCart.length;
     }
 
-        private RegionBootstrap regionBootstrap;
-
-        @Override
-        public void didDetermineStateForRegion(int arg0, Region arg1) {
-            // Don't care
-        }
-
-        @Override
-        public void didEnterRegion(Region arg0) {
-            // This call to disable will make it so the activity below only gets launched the first time a beacon is seen (until the next time the app is launched)
-            // if you want the Activity to launch every single time beacons come into view, remove this call.
-            regionBootstrap.disable();
-            Intent intent = new Intent(this, MainActivity.class);
-            // IMPORTANT: in the AndroidManifest.xml definition of this activity, you must set android:launchMode="singleInstance" or you will get two instances
-            // created when a user launches the activity manually and it gets launched from here.
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            this.startActivity(intent);
-        }
-
-        @Override
-        public void didExitRegion(Region arg0) {
-            // Don't care
-            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
-                    .setSmallIcon(R.drawable.favorite_icon)
-                    .setContentTitle("Beacon Exit")
-                    .setContentText("You have just exited the beacon region.")
-                    .setAutoCancel(true)
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-            NotificationManagerCompat.from(this).notify(0, mBuilder.build());
-
-        }
 
 }
